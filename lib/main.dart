661faf7221b6bfe19abbaab629a2919287ef738e
hugo-a-garcia <hugo.a.garcia@gmail.com>
2024-07-14 08:53:58 +0200

    Added Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:beer_json_app/scrrens/frame_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
@@ -11,7 +12,7 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
