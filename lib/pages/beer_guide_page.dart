e7c76bdc54da57721ac17d1b54f2415108a34c25
hugo-a-garcia <hugo.a.garcia@gmail.com>
2024-07-15 11:58:23 +0200

    Changed StatelessWidgget to ConsumerWidget

--- lib/pages/beer_guide_page.dart
+++ lib/pages/beer_guide_page.dart
@@ -3,32 +3,33 @@ import 'package:beer_json_app/providers/guid_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class BeerGuide extends StatelessWidget {
+class BeerGuide extends ConsumerWidget {
   const BeerGuide({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer(builder: (context, ref, childe) {
-      final AsyncValue<BJCP21> bjcp21 = ref.watch(bjcp21Provider);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<BJCP21> bjcp21 = ref.watch(bjcp21Provider);
 
-      return Center(
-          child: switch (bjcp21) {
+    return Center(
+      child: switch (bjcp21) {
         AsyncData(:final value) => ListView.builder(
             itemCount: value.beerjson.styles.length,
             itemBuilder: (context, index) {
-              return Row(children: [
-                Text(value.beerjson.styles[index].categoryId),
-                const SizedBox(width: 5),
-                Text(value.beerjson.styles[index].category),
-                const SizedBox(width: 5),
-                Text(value.beerjson.styles[index].styleId),
-                const SizedBox(width: 5),
-                Text(value.beerjson.styles[index].name),
-              ]);
+              return Row(
+                children: [
+                  Text(value.beerjson.styles[index].categoryId),
+                  const SizedBox(width: 5),
+                  Text(value.beerjson.styles[index].category),
+                  const SizedBox(width: 5),
+                  Text(value.beerjson.styles[index].styleId),
+                  const SizedBox(width: 5),
+                  Text(value.beerjson.styles[index].name),
+                ],
+              );
             }),
         AsyncError() => const Text('Oops, something unexpected happened'),
         _ => const CircularProgressIndicator(),
-      });
-    });
+      },
+    );
   }
 }
