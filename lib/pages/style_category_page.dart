26cb972d120abfae0e2a6f9695c224b70f0cc78f
hugo-a-garcia <hugo.a.garcia@gmail.com>
2024-07-22 11:32:39 +0200

    rename file

--- lib/pages/style_category_page.dart
+++ lib/pages/style_category_page.dart
@@ -1,5 +1,5 @@
 import 'package:beer_json_app/model/beer_json/style_category.dart';
-import 'package:beer_json_app/providers/guid_provider.dart';
+import 'package:beer_json_app/providers/bjcp_provider.dart';
 import 'package:beer_json_app/widgets/menu_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
